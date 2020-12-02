b4b54aee7cf7b3410290c947c6a074528b115339
angelynaliem <angelynaliem@gmail.com>
2020-12-01 23:31:50 -0500

    Need to complete logout middleware

--- api/server.js
+++ api/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 const helmet = require("helmet");
 const cors = require("cors");
 
 const usersRouter = require("../users/users-router.js");
+const authRouter = require("../auth/auth-router.js");
 
 const server = express();
 
@@ -10,7 +13,29 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+const sessionConfig = {
+  name: "alsession",
+  secret: "keep it dark",
+  cookie: {
+    maxAge: 3600 * 1000, // 1000 milliseconds
+    secure: false,
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitalized: false, //prompt to save cookies
+
+  store: new KnexSessionStore({
+    knex: require("../database/connection.js"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 3600 * 1000, //number of milliseconds in an hour
+  }), //provide access to express-session to store data to database
+};
+
+server.use(session(sessionConfig));
 server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
---
+++ auth/auth-router.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+
+const users = require("../users/users-model.js");
+
+const bcrypt = require("bcryptjs");
+
+//Registration endpoint
+router.post("/register", async (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); //2 to the 10 power or factor of 10
+
+  user.password = hash;
+
+  try {
+    const saved = await users.add(user);
+    res.status(201).json(saved);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  let { username, password } = req.body;
+
+  try {
+    const user = await users.findBy({ username }).first();
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.session.user = user;
+      res.status(200).json({ message: `Welcome ${user.username}` });
+    } else {
+      res.status(401).json({ message: "invalid creds" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get("/logout", (req, res) => {
+  if (req.session && req.session.user) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.send();
+      }
+    });
+  }
+});
+
+module.exports = router;
